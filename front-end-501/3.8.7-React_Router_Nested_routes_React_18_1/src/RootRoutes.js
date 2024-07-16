fdccdd96b15e3d6521aef2c16c9d622eab0aa60f
gchai <g@bechai.com>
2024-07-16 00:24:12 -0400

    Nested Routes. This was silly.

--- front-end-501/3.8.7-React_Router_Nested_routes_React_18_1/src/RootRoutes.js
+++ front-end-501/3.8.7-React_Router_Nested_routes_React_18_1/src/RootRoutes.js
@@ -2,13 +2,22 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Users from "./Users";
 import User from "./User";
+import NoRoute from './NoRoute';
+import About from './About';
+import UserPosts from './UserPosts';
+import UserPost from './UserPost';
+import UserProfile from './UserProfile';
 
 function RootRoutes() {
   return (
     <Routes>
-    <Route path="/*" element={<Users />} />
-    <Route path="/users/:userId/*" element={<User />} />
-    </Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/users/:userId/posts/:postId" element={<UserPost /> } />
+        <Route path="/users/:userId/posts" element={<UserPosts /> } />
+        <Route path="/users/:userId/*" element={<User />}/>
+        <Route path="*" element={<NoRoute />} />
+  </Routes>
   )
 }
 
