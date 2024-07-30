b0516686a4b50ae45c418b52f01b20b9ad731c03
gchai <github@gabechai.com>
2024-07-30 14:33:59 -0400

    Redux assignment fix

--- front-end-501/3.12.6-Assessment_Redux/src/features/suggestion/suggestion.slice.js
+++ front-end-501/3.12.6-Assessment_Redux/src/features/suggestion/suggestion.slice.js
@@ -1,42 +1,43 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+/* √ Task 15: Complete the `createAsyncThunk()` function to load a suggestion from this URL: http://localhost:3004/api/suggestion */
 export const fetchSuggestion =
-  createAsyncThunk(/* √ Task 15: Complete the `createAsyncThunk()` function to load a suggestion from this URL: http://localhost:3004/api/suggestion */
-    `suggestion/fetch`,
+  createAsyncThunk(
+    `suggestion/fetchSuggestion`,
     async () => {
       const response = await fetch("http://localhost:3004/api/suggestion");
-      const data = response.json();
-      return data.data
+      const {data} = await response.json();
+      return data
     }
   );
 
-const initialState = {
-  suggestion: '',
-  loading: false,
-  error: true,
-};
+  const initialState = {
+    suggestion: '',
+    loading: false,
+    error: true,
+  };
 
-const options = {
-  name: 'suggestion',
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    /* √ Task 16: Inside `extraReducers`, add reducers to handle all three promise lifecycle states - pending, fulfilled, and rejected - for the `fetchSuggestion()` call */
-    builder
-      .addCase(fetchSuggestion.pending, (state, action) => ({
-        ...state,
-        loading: true,
-        error: action.error.message }))
-      .addCase(fetchSuggestion.fulfilled, (state, action) => ({
-        ...state,
-        suggestion: action.payload,
-        loading: false }))
-      .addCase(fetchSuggestion.rejected, (state, action) => ({
-        ...state,
-        loading: false,
-        error: action.error.message }))
-},
-};
+  const options = {
+    name: 'suggestion',
+    initialState,
+    reducers: {},
+    extraReducers: {
+      /* √ Task 16: Inside `extraReducers`, add reducers to handle all three promise lifecycle states - pending, fulfilled, and rejected - for the `fetchSuggestion()` call */
+      [fetchSuggestion.pending]: (state) => {
+        state.loading = true;
+        state.error = false;
+      },
+      [fetchSuggestion.fulfilled]: (state, { payload }) => {
+        state.suggestion = payload;
+        state.loading = false;
+        state.error = false;
+      },
+      [fetchSuggestion.rejected]: (state, action) => {
+        state.loading = false;
+        state.error = true;
+      },
+    },
+  };
 
 const suggestionSlice = createSlice(options);
 
