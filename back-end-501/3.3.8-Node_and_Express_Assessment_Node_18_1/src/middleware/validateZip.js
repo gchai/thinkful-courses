ca1d46a1a8256dde87fc20d6a604f4a5a3234acf
gchai <github@gabechai.com>
2024-08-29 12:25:36 -0400

    Express assessment complete

--- back-end-501/3.3.8-Node_and_Express_Assessment_Node_18_1/src/middleware/validateZip.js
+++ back-end-501/3.3.8-Node_and_Express_Assessment_Node_18_1/src/middleware/validateZip.js
@@ -1,3 +1,10 @@
-function validateZip(req, res, next) {}
+function validateZip(req, res, next) {
+    const zip = req.params.zip
+    if (zip.length===5 && !isNaN(zip)) {
+        next()
+    } else {
+        next(`Zip (${zip}) is invalid!`)
+    }
+}
 
 module.exports = validateZip;
