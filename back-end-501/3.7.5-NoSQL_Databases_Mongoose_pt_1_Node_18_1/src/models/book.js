6a1f44cac23d953a5a286916c21368427c6a77ec
gchai <g@bechai.com>
2024-09-03 16:06:57 -0400

    3.7.5-Mongoose pt 1 done

--- back-end-501/3.7.5-NoSQL_Databases_Mongoose_pt_1_Node_18_1/src/models/book.js
+++ back-end-501/3.7.5-NoSQL_Databases_Mongoose_pt_1_Node_18_1/src/models/book.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const bookSchema = mongoose.Schema({
- // TODO: Write your code here.
-
+    title: String,
+    author: String,
+    description: String,
+    cost: Number,
 });
 
 const Book = mongoose.model("Book", bookSchema);
