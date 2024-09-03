6282e844aff64dc2b48a58eeec29ccf95621f330
gchai <github@gabechai.com>
2024-09-03 15:10:01 -0400

    Mongodb assessment init

---
+++ back-end-501/3.7.7-NoSQL_Databases_Assessment_Node_18_1/src/weather/weather.model.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+
+const schema = mongoose.Schema({
+  // TODO: add day (Date), city (String), state(String), country (String), temperature (Number), and condition (String)
+  
+  
+})
+
+module.exports = mongoose.model("WeatherStatus", schema)
