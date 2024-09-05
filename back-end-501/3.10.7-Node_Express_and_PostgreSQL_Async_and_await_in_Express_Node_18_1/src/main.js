8e23c7fb833ef3dacba7b7f745ea38c0b21dd82d
gchai <g@bechai.com>
2024-09-04 20:54:56 -0400

    3.10.7-Express Postress Async complete

--- back-end-501/3.10.7-Node_Express_and_PostgreSQL_Async_and_await_in_Express_Node_18_1/src/main.js
+++ back-end-501/3.10.7-Node_Express_and_PostgreSQL_Async_and_await_in_Express_Node_18_1/src/main.js
@@ -4,19 +4,25 @@ const {
   generateSlogan,
 } = require("../utils/slogan-generator");
 
-function getSlogan(request) {
-  generateSlogan(request).then(response => {
+async function getSlogan(request) {
+  try {
+    const response = await generateSlogan(request);
     console.log(`Your request was: ${request}`);
     console.log(`Your slogan is: ${response.slogan}`);
-  });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function fullSession(request) {
-  generateMessage()
-    .then(console.log)
-    .then(() => getSlogan(request))
-    .then(() => goodbye())
-    .then(console.log);
+async function fullSession(request) {
+  try {
+    console.log(await generateMessage());
+    await getSlogan(request);
+    console.log("Best of luck on your startup...");
+    await goodbye();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = { getSlogan, fullSession };
