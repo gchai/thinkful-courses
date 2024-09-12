d4be2e5f8a88893c04039132ab54547f8e05733d
gchai <g@bechai.com>
2024-09-12 13:59:35 -0400

    Init

---
+++ back-end-501/Mock_Interview_Backend_Guild_1/src/customers/customers.router.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const controller = require("./customers.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+// Add the appropriate methods in this file
+// Hint: You can use router.route() to chain route methods that share the same path  
+module.exports = router;
