7d74b0e057112895abeba844cffa35d758bae748
gchai <g@bechai.com>
2024-09-12 13:13:32 -0400

    WIP

--- back-end-501/3.15.2-Final_Capstone_WeLoveMovies_Guild_Node_18_1/src/movies/movies.controller.js
+++ back-end-501/3.15.2-Final_Capstone_WeLoveMovies_Guild_Node_18_1/src/movies/movies.controller.js
@@ -2,19 +2,29 @@ const service = require("./movies.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function movieExists(request, response, next) {
-  // TODO: Add your code here.
-
-  next({});
+  const { movieId } = request.params;
+  const movie = await service.read(movieId);
+  if (movie) {
+    response.locals.movie = movie;
+    return next();
+  }
+  next({ status: 404, message: "Movie not found" });
 }
 
 async function read(request, response) {
-  // TODO: Add your code here
-  response.json({ data: "" });
+  const { movieId } = request.params;
+  const data = await service.read(movieId);
+  if (data) {
+    response.json({ data });
+  } else {
+    response.status(404).json({ error: "Movie not found" });
+  }
 }
 
 async function list(request, response) {
-  // TODO: Add your code here.
-  response.json({ });
+  const { is_showing } = request.query;
+  const data = await service.list(is_showing);
+  response.json({ data });
 }
 
 module.exports = {
