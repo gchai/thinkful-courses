9b77767c6b95855e5b712f081e48ab1e26e54f33
gchai <g@bechai.com>
2024-09-19 11:12:33 -0400

    Fixing migrations

--- back-end-501/3.15.2-Final_Capstone_WeLoveMovies_Guild_Node_18_1/src/db/migrations/05_createMovieTheaters.js
+++ back-end-501/3.15.2-Final_Capstone_WeLoveMovies_Guild_Node_18_1/src/db/migrations/05_createMovieTheaters.js
@@ -1,14 +1,21 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("movies", (table) => {
-    table.increments("movie_id").primary();
-    table.string("title");
-    table.integer("runtime_in_minutes");
-    table.string("rating");
-    table.text("description");
-    table.string("image_url");
+  return knex.schema.createTable("movies_theaters", (table) => {
+    table.integer("movie_id").unsigned().notNullable();
+    table
+      .foreign("movie_id")
+      .references("movie_id")
+      .inTable("movies")
+      .onDelete("CASCADE");
+    table.integer("theater_id").unsigned().notNullable();
+    table
+      .foreign("theater_id")
+      .references("theater_id")
+      .inTable("theaters")
+      .onDelete("CASCADE");
+    table.boolean("is_showing");
   });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("movies");
+  return knex.schema.dropTable("movies_theaters");
 };
