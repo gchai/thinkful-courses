a2101661bf3f801c71f0527b92c267349e2ebbbd
gchai <g@bechai.com>
2024-10-08 15:10:49 -0400

    DSA Arrays done

--- DSA-501/1.3.2-Data_structures_Arrays_Node_18_1/src/missingNumber.js
+++ DSA-501/1.3.2-Data_structures_Arrays_Node_18_1/src/missingNumber.js
@@ -1,11 +1,22 @@
 /**
  * Implement a brute force algorithm for finding the missing number in an array
  */
-function missingNumberBruteForce(numbers) {}
+function missingNumberBruteForce(numbers) {
+    for (let i=1;i<=numbers.length+1;i++){
+        if (!numbers.includes(i)){
+            return i
+        }
+    }
+}
 
 /**
  * Use an iterative  strategy for finding the missing number in an array
  */
-function missingNumberSum(numbers) {}
+function missingNumberSum(numbers) {
+    let n = numbers.length+1;
+    let expected = n*(n+1)/2;
+    let actual = numbers.reduce((acc,curr) => acc + curr);
+    return expected-actual;
+}
 
 module.exports = { missingNumberBruteForce, missingNumberSum };
