1dec145cf2fae0f4413998541f86cf3ff3226d0a
gchai <github@gabechai.com>
2024-10-09 17:34:31 -0400

    1.3.9-Queues and Stacks 2 complete

--- DSA-501/1.3.9-Data_structures_Solving_problems_with_stacks_and_queues_Node_18_1/src/binary.js
+++ DSA-501/1.3.9-Data_structures_Solving_problems_with_stacks_and_queues_Node_18_1/src/binary.js
@@ -1,3 +1,16 @@
-const binary = (max) => {};
+const Queue = require("./lib/queue");
 
+const binary = (max) => {
+  const numbers = new Queue();
+  numbers.enqueue("1");
+  let result = [];
+
+  for (let i = 1; i <= max; i++) {
+    let newNumber = numbers.dequeue();
+    result.push(newNumber);
+    numbers.enqueue(newNumber.concat("0"));
+    numbers.enqueue(newNumber.concat("1"));
+  }
+  return result;
+};
 module.exports = binary;
--- DSA-501/1.3.9-Data_structures_Solving_problems_with_stacks_and_queues_Node_18_1/src/brackets.js
+++ DSA-501/1.3.9-Data_structures_Solving_problems_with_stacks_and_queues_Node_18_1/src/brackets.js
@@ -1,5 +1,31 @@
 const Stack = require("./lib/stack");
 
-const match = (expression) => {};
+const match = (expression) => {
+  const newStack = new Stack();
+  for (let index = 0, limit = expression.length; index < limit; index++) {
+    switch (expression[index]) {
+      case "(":
+      case "[":
+      case "{":
+        newStack.push(expression[index]);
+        break;
+      case ")":
+        if (newStack.top?.value === "(") {
+          newStack.pop();
+        } else return false;
+        break;
+      case "]":
+        if (newStack.top?.value === "[") {
+          newStack.pop();
+        } else return false;
+        break;
+      case "}":
+        if (newStack.top?.value === "{") {
+          newStack.pop();
+        } else return false;
+    }
+  }
+  return !newStack.top;
+};
 
 module.exports = match;
