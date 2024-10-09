9aa5b82f6ec751a0857b43df3fb77b62cce6e772
gchai <github@gabechai.com>
2024-10-09 16:12:54 -0400

    1.3.3 Sets and Maps done

--- DSA-501/1.3.3-Data_structures_Sets_and_Maps_Node_18_1/src/equalArrays.js
+++ DSA-501/1.3.3-Data_structures_Sets_and_Maps_Node_18_1/src/equalArrays.js
@@ -1,6 +1,26 @@
 /**
  * return true if two arrays are equal, false otherwise
  */
-function isEqual(a1, a2) {}
+function isEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  const countMap = new Map();
+  for (const num of arr1) {
+    countMap.set(num, (countMap.get(num) || 0) + 1);
+  }
+  for (const num of arr2) {
+    if (!countMap.has(num)) {
+      return false;
+    }
+    const count = countMap.get(num);
+    if (count === 1) {
+      countMap.delete(num);
+    } else {
+      countMap.set(num, count - 1);
+    }
+  }
+  return countMap.size === 0;
+}
 
 module.exports = isEqual;
--- DSA-501/1.3.3-Data_structures_Sets_and_Maps_Node_18_1/src/setOperations.js
+++ DSA-501/1.3.3-Data_structures_Sets_and_Maps_Node_18_1/src/setOperations.js
@@ -1,16 +1,27 @@
 /**
  * return the union of two sets
  */
-function union(s1, s2) {}
-
+function union(s1, s2) {
+  let newSet = [...s1, ...s2];
+  return new Set(newSet);
+}
 /**
  * return the intersection of two sets
  */
-function intersect(s1, s2) {}
-
+function intersect(s1, s2) {
+  return new Set([...s1].filter((x) => s2.has(x)));
+}
 /**
  * return the difference of two sets
  */
-function difference(s1, s2) {}
+function difference(setA, setB) {
+  const diff = new Set();
+  for (const element of setA) {
+    if (!setB.has(element)) {
+      diff.add(element);
+    }
+  }
+  return diff;
+}
 
 module.exports = { union, intersect, difference };
