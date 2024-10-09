2d959bf08f94af00f8170785e6540fb60ad8f01b
gchai <github@gabechai.com>
2024-10-09 17:09:32 -0400

    1.3.7 Linked Lists pt 2 complete

--- DSA-501/1.3.7-Data_structures_Solving_problems_with_linked_lists_Node_18_1/src/editor.js
+++ DSA-501/1.3.7-Data_structures_Solving_problems_with_linked_lists_Node_18_1/src/editor.js
@@ -19,29 +19,54 @@ class Editor {
    * @param {*} char a value to be inserted into the editor
    * @returns {Editor} a reference to this editor
    */
-  insert() {}
-
+  insert(char) {
+    if (this.cursor) {
+      this.text.insert(char, (node) => node.value === this.cursor.value);
+    } else {
+      this.text.insertAtHead(char);
+    }
+    return this.arrowRight();
+  }
   /**
    * Remove the character at the cursor position.
    * Moves the cursor to the previous position.
    * If editor is empty does nothing.
    * @returns {Editor} a reference to this editor
    */
-  delete() {}
-
+  delete() {
+    if (this.cursor) {
+      const location = this.cursor;
+      this.arrowLeft();
+      this.text.remove((node) => node === location);
+    }
+    return this;
+  }
   /**
    * Moves the cursor one position to the left.
    * If the cursor is at the start of the editor nothing happens.
    * @returns {Editor} a reference to this editor
    */
-  arrowLeft() {}
-
+  arrowLeft() {
+    if (this.cursor && this.text.head) {
+      this.cursor = this.text.findWithPrevious(
+        (node) => this.cursor.value === node.value
+      )[1];
+    }
+    return this;
+  }
   /**
    * Moves the cursor one position to the right.
    * If the cursor is at the end of the editor nothing happens.
    * @returns {Editor} a reference t this editor
    */
-  arrowRight() {}
+  arrowRight() {
+    if (this.cursor && this.cursor.next) {
+      this.cursor = this.cursor.next;
+    } else if (!this.cursor) {
+      this.cursor = this.text.head;
+    }
+    return this;
+  }
 }
 
 module.exports = Editor;
