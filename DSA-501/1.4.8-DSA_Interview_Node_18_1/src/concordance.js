930267b424f7372836e5aa7355ba1e97372156c0
gchai <github@gabechai.com>
2024-10-09 16:07:07 -0400

    1.4.8 DSA Mock Interview Complete

--- DSA-501/1.4.8-DSA_Interview_Node_18_1/src/concordance.js
+++ DSA-501/1.4.8-DSA_Interview_Node_18_1/src/concordance.js
@@ -4,6 +4,26 @@
  If the same word appears multiple times on a line it should
  list the line only once.
 */
-function concordance(data) {}
+function concordance(data) {
+  const concordance = new Map();
+  
+  data.forEach((line, num)=> {
+    const words = line.split(/[\s.,';]/).filter(word => word !==``)
+    
+    words.forEach(word => {
+      const lowerWord = word.toLowerCase();
+      
+      if(!concordance.has(lowerWord)){
+        concordance.set(lowerWord, [num]);
+      } else if (!concordance.get(lowerWord).includes(num)){
+        concordance.get(lowerWord).push(num);
+      }
+      
+      
+    })
+  })
+  return Object.fromEntries(concordance);
+}
 
 module.exports = concordance;
+
--- DSA-501/1.4.8-DSA_Interview_Node_18_1/src/searchLines.js
+++ DSA-501/1.4.8-DSA_Interview_Node_18_1/src/searchLines.js
@@ -6,6 +6,18 @@
   Return:  
     an array of all sentences containing words in the list
 */
-function searchLines(words, concordance, data) {}
+function searchLines(words, concordance, data) {
+  const lines = new Set();
+  
+  for (let currNode = words.head; currNode; currNode = currNode.next){
+    const word = currNode.value.toLowerCase();
+    if (concordance[word]){
+      concordance[word].forEach(lineNum => {
+        lines.add(data[lineNum])
+      })
+    }
+  }
+  return [...lines]
+}
 
 module.exports = searchLines;
