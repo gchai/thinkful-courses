18f124b23dd54774f805085b4536da8ebe3d8c93
gchai <github@gabechai.com>
2024-10-09 13:56:49 -0400

    DSA Mock Interview

---
+++ DSA-501/1.4.8-DSA_Interview_Node_18_1/src/concordance.js
@@ -0,0 +1,9 @@
+/*
+ Given an array of sentences making up a body of text,
+ output a concordance of words that appear in the text.
+ If the same word appears multiple times on a line it should
+ list the line only once.
+*/
+function concordance(data) {}
+
+module.exports = concordance;
---
+++ DSA-501/1.4.8-DSA_Interview_Node_18_1/src/searchLines.js
@@ -0,0 +1,11 @@
+/*
+  Given:
+    a linked list of words
+    a concordance
+    the original data set
+  Return:  
+    an array of all sentences containing words in the list
+*/
+function searchLines(words, concordance, data) {}
+
+module.exports = searchLines;
