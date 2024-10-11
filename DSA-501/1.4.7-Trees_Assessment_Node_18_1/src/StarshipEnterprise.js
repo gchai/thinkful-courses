baefb6d238ca394097135f6877d39cdeb71226dc
gchai <github@gabechai.com>
2024-10-11 04:31:07 -0400

    1.4.7 Trees Assessment done

--- DSA-501/1.4.7-Trees_Assessment_Node_18_1/src/StarshipEnterprise.js
+++ DSA-501/1.4.7-Trees_Assessment_Node_18_1/src/StarshipEnterprise.js
@@ -10,21 +10,77 @@ class StarshipEnterprise {
   }
 
   assignOfficer(officerId, officerName) {
-    // your solution here
+    if (this.officerId == null) {
+      this.officerId = officerId;
+      this.officerName = officerName;
+    } else if (officerId < this.officerId) {
+      if (this.leftReport == null) {
+        this.leftReport = new StarshipEnterprise(officerId, officerName, this);
+      } else {
+        this.leftReport.assignOfficer(officerId, officerName);
+      }
+    } else {
+      if (this.rightReport == null) {
+        this.rightReport = new StarshipEnterprise(officerId, officerName, this);
+      } else {
+        this.rightReport.assignOfficer(officerId, officerName);
+      }
+    }
   }
 
   findOfficersWithNoDirectReports(values = []) {
-    // your solution here
+    if (!this.leftReport && !this.rightReport) {
+      values.push(this.officerName);
+    }
+    if (this.leftReport) {
+      values = this.leftReport.findOfficersWithNoDirectReports(values);
+    }
+    if (this.rightReport) {
+      values = this.rightReport.findOfficersWithNoDirectReports(values);
+    }
     return values;
   }
 
   listOfficersByExperience(officerNames = []) {
-    // your solution here
+    if (this.leftReport) {
+      officerNames = this.leftReport.listOfficersByExperience(officerNames);
+    }
+    officerNames.unshift(this.officerName);
+    if (this.rightReport) {
+      officerNames = this.rightReport.listOfficersByExperience(officerNames);
+    }
     return officerNames;
   }
 
   listOfficersByRank(tree, rankedOfficers = {}) {
-    // your solution here
+    const queue = new Queue();
+    queue.enqueue(tree);
+    let node = queue.dequeue();
+    let rank = 1;
+    rankedOfficers = { [rank]: [node.officerName] };
+    while (node) {
+      if (
+        node.reportTo &&
+        rankedOfficers[rank].includes(node.reportTo.officerName)
+      ) {
+        rank = rank + 1;
+        rankedOfficers[rank] = [node.officerName];
+      }
+      if (
+        node.reportTo &&
+        rankedOfficers[rank - 1].includes(node.reportTo.officerName) &&
+        !rankedOfficers[rank].includes(node.officerName)
+      ) {
+        rankedOfficers[rank] = [...rankedOfficers[rank], node.officerName];
+      }
+      if (node.leftReport) {
+        queue.enqueue(node.leftReport);
+      }
+      if (node.rightReport) {
+        queue.enqueue(node.rightReport);
+      }
+      node = queue.dequeue();
+    }
     return rankedOfficers;
   }
 }
