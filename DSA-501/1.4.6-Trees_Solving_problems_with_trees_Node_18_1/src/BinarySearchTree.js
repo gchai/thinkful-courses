af83b7cbdf46c66f8f6d9622d22062d6dd523658
gchai <github@gabechai.com>
2024-10-10 22:55:50 -0400

    1.4.6 Tree solving problems done

--- DSA-501/1.4.6-Trees_Solving_problems_with_trees_Node_18_1/src/BinarySearchTree.js
+++ DSA-501/1.4.6-Trees_Solving_problems_with_trees_Node_18_1/src/BinarySearchTree.js
@@ -145,13 +145,26 @@ class BinarySearchTree {
   }
 
   countLeaves(count = 0) {
-    // your solution here
-    return count;
+    if (!this.left && !this.right) return count + 1;
+    const newCount = count + 1;
+    if (!this.left) return this.right.countLeaves(newCount);
+    if (!this.right) return this.left.countLeaves(newCount);
+    const leftCount = this.left.countLeaves(newCount);
+    const rightCount = this.right.countLeaves(newCount);
+    return Math.max(leftCount, rightCount);
   }
 
-  isBalancedBST() {
-    // your solution here
-    return 0;
+  isBalancedBST(currentHeight = 0) {
+    if (!this.left && !this.right) return currentHeight;
+    const newHeight = currentHeight + 1;
+    if (!this.left) return this.right.getHeight(newHeight);
+    if (!this.right) return this.left.getHeight(newHeight);
+    const leftHeight = this.left.getHeight(newHeight);
+    const rightHeight = this.right.getHeight(newHeight);
+    const taller = Math.max(leftHeight, rightHeight);
+    const shorter = Math.min(leftHeight, rightHeight);
+    const diff = taller - shorter;
+    return diff >= -1 && diff <= 1 ? taller : -1;
   }
 
   _replaceWith(node) {
